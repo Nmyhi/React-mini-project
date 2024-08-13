a720c4228a944409ea5d83c3023446ed2d45a986
Mihaly Lovrencsics <lomysa@gmail.com>
2024-08-13 11:04:23 +0000

    Add ES7snippets and notes for snippets

---
+++ my-app/src/components/ES7Snippets.js
@@ -0,0 +1,51 @@
+//rfce snippet
+
+//import React from 'react'
+//
+//function ES7Snippets() {
+// return (
+//    <div>ES7Snippets</div>
+//  )
+//}
+
+//export default ES7Snippets
+
+//rafce snippet
+
+//import React from 'react'
+//
+//const ES7Snippets = () => {
+//  return (
+//   <div>ES7Snippets</div>
+//  )
+//}
+
+//export default ES7Snippets
+
+//rce snippet
+
+//import React, { Component } from 'react'
+
+//export class ES7Snippets extends Component {
+//  render() {
+//    return (
+//      <div>ES7Snippets</div>
+//    )
+//  }
+//}
+//
+//export default ES7Snippets
+
+//rconst snippet
+
+//constructor(props) {
+//  super(props)
+//
+//  this.state = {
+//     first
+//  }
+//}
+
+//clg snippet
+
+//console.log(first)
