b314607b439d23fb4067ff82a175eed4e225b24f
Mihaly Lovrencsics <lomysa@gmail.com>
2024-08-13 21:31:31 +0000

    Add onclick attribute and a function to a functional component and a class component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,8 @@ import FunctionalGreeting from './components/FunctionalGreeting';
 import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
 import StatefulGreeting from './components/StatefulGreeting';
 import StatefulGreetingWithCallback from './components/StatefulGreetingsWithCallback';
+import EventsFunctional from './components/EventsFunctional';
+import EventsClass from './components/EventsClass';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <FunctionalGreetingWithProps greeting="nice to meet you!!" name="Mike" age="32" />
       <StatefulGreeting greeting="I am a stateful class component!" name="Mish"/>
       <StatefulGreetingWithCallback/>
+      <EventsFunctional/>
+      <EventsClass/>
     </div>
   );
 }
