fad083cc1e71b0f8aa161dba45251f68181af8cd
Mihaly Lovrencsics <lomysa@gmail.com>
2024-08-20 16:35:33 +0000

    Add conditional formatting with props and construstor

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
     <ConditionalRenderingClass/>
-    <ConditionalRenderingFunctional/> 
+    <ConditionalRenderingFunctional connected={true}/> 
     </div>
   );
 }
--- my-app/src/components/ConditionalRenderingClass.js
+++ my-app/src/components/ConditionalRenderingClass.js
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { Component } from 'react'
 
+export class ConditionalRenderingClass extends Component {
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         isloaded: true,
+         isLoggedIn: true
+      }
+    }
 
-
-
-
-
-
-
-
+  render() {
+    return (
+      <div>
+        <h1>{this.state.isloaded ? "Data loaded!" : "Loading..."}</h1>
+        {this.state.isLoggedIn ? (
+            <div>
+                <p>Welcome to the site, please complete the steps below:</p>
+                <ol>
+                    <li>Confirm your email!</li>
+                    <li>Complete your profile!</li>
+                    <li>Subcribe to the newsletter!</li>
+                </ol>  
+            </div>
+        ) : 
+        ( <p>Please sign in!</p>)}
+      </div>
+    )
+  }
+}
 
 export default ConditionalRenderingClass;
