799a17309d8feb77ee9820deddd870bf908c5b2d
Mihaly Lovrencsics <lomysa@gmail.com>
2024-08-20 13:55:23 +0000

    Add ConditionalRenderingClass and ConditionalRenderingFunctional to the Repo, modify app.js accordingly

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,22 +1,13 @@
 import './App.css';
-import FunctionalGreeting from './components/FunctionalGreeting';
-import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
-import StatefulGreeting from './components/StatefulGreeting';
-import StatefulGreetingWithCallback from './components/StatefulGreetingsWithCallback';
-import EventsFunctional from './components/EventsFunctional';
-import EventsClass from './components/EventsClass';
-import EventBinding from './components/EventBinding';
+import ConditionalRenderingClass from './components/ConditionalRenderingClass';
+import ConditionalRenderingFunctional from './components/ConditionalRenderingFunctional';
+
 
 function App() {
   return (
     <div className="App">
-    <FunctionalGreeting/>
-    <FunctionalGreetingWithProps greeting="nice to meet you!!" name="Mike" age="32" />
-    <StatefulGreeting greeting="I am a stateful class component!" name="Mish"/>
-    <StatefulGreetingWithCallback/>
-    <EventsFunctional/>
-    <EventsClass/>
-    <EventBinding/>
+    <ConditionalRenderingClass/>
+    <ConditionalRenderingFunctional/> 
     </div>
   );
 }
