943e7e3e34854eee0a1ba1e792f5b62e8424d778
Mihaly Lovrencsics <lomysa@gmail.com>
2024-08-24 20:22:41 +0000

    Add sign in and sign out buttons and methods

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,12 @@
 import './App.css';
 import MethodsAsParent from './components/MethodsAsParent';
-import MethodsAsPropsChild from './components/MethodsAsPropsChild';
+
 
 
 function App() {
   return (
     <div className="App">
       <MethodsAsParent/>
-      <MethodsAsPropsChild/>
     </div>
   );
 }
--- my-app/src/components/MethodsAsParent.js
+++ my-app/src/components/MethodsAsParent.js
@@ -5,13 +5,30 @@ class MethodsAsPropsParent extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            isLoggedIn: true,
+            isLoggedIn: false,
         }
     }
+
+handleSignIn = () => {
+    this.setState({
+        isLoggedIn: true
+    });
+}
+
+handleSignOut = () => {
+    this.setState({
+        isLoggedIn: false
+    })
+}
+
     render() {
         return (
             <div>
-                <MethodsAsPropsChild isLoggedIn={this.state.isLoggedIn} />
+                <MethodsAsPropsChild 
+                isLoggedIn={this.state.isLoggedIn}
+                handleSignIn={this.handleSignIn}
+                handleSignOut={this.handleSignOut}
+                />
             </div>
         )
     }
