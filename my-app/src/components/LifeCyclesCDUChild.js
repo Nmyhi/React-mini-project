7e8725eabffb0c4e942d656bd53185fc3e083065
Mihaly Lovrencsics <lomysa@gmail.com>
2024-08-31 22:26:19 +0000

    Add 2 new components

---
+++ my-app/src/components/LifeCyclesCDUChild.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class LifeCyclesCDUChild extends Component {
+  render() {
+    return (
+      <div>LifeCyclesCDUChild</div>
+    )
+  }
+}
+
+export default LifeCyclesCDUChild
