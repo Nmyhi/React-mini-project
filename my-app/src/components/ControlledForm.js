699cc3ab03d768e1d9a023b0ad72aae0149a72de
Mihaly Lovrencsics <lomysa@gmail.com>
2024-09-02 21:45:42 +0000

    Add form elements , prevent default behaviour on submitting

--- my-app/src/components/ControlledForm.js
+++ my-app/src/components/ControlledForm.js
@@ -5,7 +5,9 @@ export class ControlledForm extends Component {
       super(props)
     
       this.state = {
-         name: ''
+         name: '',
+         category: 'website',
+         comments: '',
       }
     }
     handleNameChange = (event) => {
@@ -13,12 +15,41 @@ export class ControlledForm extends Component {
             name: event.target.value
         })
     }
+    handleCategoryChange = (event) => {
+      this.setState({
+        category: event.target.value
+      })
+    }
+    handleCommentsChange = (event) => {
+      this.setState({
+        comments: event.target.value
+      })
+    }
+    handleSubmit = (event) => {
+      event.preventDefault();
+      console.log(this.state)
+    }
   render() {
     return (
       <div>
-        <form>
+        <h2>Please fill out the form below:</h2>
+        <form onSubmit={this.handleSubmit}>
+          <div>
             <label htmlFor="id-name">Your Name:</label>
             <input value={this.state.name} onChange={this.handleNameChange} id="id-name" name="name" type="text"></input>
+          </div>
+          <div>
+            <label htmlFor="id-category">Query category:</label>
+            <select id="id-categtory" name="category" value={this.state.category} onChange={this.handleCategoryChange}>
+              <option value="website">Website issue</option>
+              <option value="order">Order issue</option>
+              <option value="general">General enquiry</option>
+            </select>
+          </div>
+          <div>
+            <label htmlFor="id-comments">Comments:</label>
+            <textarea id="id-comments" name="comments" value={this.state.comments} onChange={this.handleCommentsChange}/>
+          </div>
             <input type="submit" value="Submit"/>
         </form>
       </div>
